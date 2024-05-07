ccf395a8115d3b5a5050d507fcffa5c2b98e7201
Abhishek <kusabiabhishek@gmail.com>
2024-05-07 17:45:55 +0530

    styling_changed

--- mainapp/static/script.js
+++ mainapp/static/script.js
@@ -59,46 +59,6 @@ function updateValue(){
     });
 }
 
-// Function to GET csrftoken from Cookie
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-var csrftoken = getCookie('csrftoken');
-
-
-
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-// Function to set Request Header with `CSRFTOKEN`
-function setRequestHeader(){
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-}
-
-'use strict';
-
   // Function to scroll to a specific section when a navbar link is clicked
   function scrollToSection(sectionId) {
     document.getElementById(sectionId).scrollIntoView({
@@ -124,3 +84,4 @@ function scrollToTop() {
       behavior: 'smooth'
   });
 }
+
